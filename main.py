671e95454e10d1139e63dcb3652333c58767f3ee
WarmMilkCodes <mrguybanks@gmail.com>
2023-03-01 11:47:13 -0600

    Add event viewer route / page

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, json
+from flask import Flask, render_template, request, redirect, url_for, json, Response
 import subprocess
 import config
 
@@ -7,6 +7,12 @@ app = Flask(__name__)
 # define username and password
 username = config.user_name
 password = config.password
+
+# define bots pm2 process IDs
+bot_processes = {
+    'bot1':0,
+    'bot2':1
+}
 
 @app.route('/')
 def index():
@@ -40,6 +46,36 @@ def status():
         })
 
     return render_template('status.html', bot_statuses=bot_statuses)
+
+@app.route('/logs/<bot_name>')
+def logs(bot_name):
+    # check if bot exists
+    if bot_name not in bot_processes:
+        return f'{bot_name} not found', 404
+
+    # get pm2 process ID for bot
+    process_id = bot_processes[bot_name]
+
+    # tail the log file for pm2 process
+    log_process = subprocess.Popen(['pm2', 'logs', str(process_id)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    # function to generate SSE events from log output
+    def log_events():
+        while True:
+            line = log_process.stdout.readline()
+            if not line:
+                break
+            yield 'data: %s\n\n' % line.decode('utf-8')
+
+    # return response with SSE headers and event generator function
+    return Response(log_events(), mimetype='text/event-stream')
+
+    if request.method == 'POST':
+        selected_bot = request.form['bot']
+        logs = subprocess.check_output(['pm2', 'logs', '--lines', '100', selected_bot]).decode('utf-8')
+        return render_template('logs.html', bots=['bot1', 'bot2', 'bot3'], logs=logs, selected_bot=selected_bot)
+    else:
+        return render_template('logs.html', bots=['bot1', 'bot2', 'bot3'])
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
